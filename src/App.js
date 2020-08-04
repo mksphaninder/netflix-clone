e3c1a69838ee62fe985dd4c71ad17e56ffdd97cd
mksphaninder <julienbi@unity3d.com>
2020-08-04 18:54:40 -0400

    - Finished building Navbar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import "./App.css";
 import Row from "./Row";
 import request from "./request";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-      {/* navbar */}
-
-      {/* banner */}
+      <Nav />
       <Banner />
       <Row
         title="Movies in theater"
